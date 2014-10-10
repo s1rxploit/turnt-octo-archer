bcac3f2865e5a5b6bec05e1f30deba2967df4184
Vincent St. Louis <vincentst.louis@gmail.com>
2014-10-10 11:52:26 -0400

    missing files

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -10,16 +10,16 @@ Route::group(['prefix'=>'api'],function()
     Route::post('auth/forgotpassword', 'Auth\RemindersController@postRemind');
 });
 
-//App::missing(function()
-//{
-//    return view('master')->with('window', new App\Helpers\JSHelper );
-//});
+App::missing(function()
+{
+    return view('master')->with('window', new App\Helpers\JSHelper );
+});
 
-//App::after(function($request, $response)
-//{
-//    if($response instanceof \Illuminate\Http\JsonResponse)
-//    {
-//        $json = ")]}',\n" . $response->getContent();
-//        return $response->setContent($json);
-//    }
-//});
+App::after(function($request, $response)
+{
+    if($response instanceof \Illuminate\Http\JsonResponse)
+    {
+        $json = ")]}',\n" . $response->getContent();
+        return $response->setContent($json);
+    }
+});
