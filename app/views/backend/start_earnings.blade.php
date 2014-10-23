b9d55424016f2ecbbe2f2d365f25fce69beefc0d
Md Imran <shellprog@gmail.com>
2014-10-24 03:21:09 +0530

    trialpay

--- app/views/backend/start_earnings.blade.php
+++ app/views/backend/start_earnings.blade.php
@@ -15,7 +15,8 @@
 		<h6 class="panel-title"><i class="icon-user-plus2"></i> Offer Wall</h6>
 	</div>
 	<div class="panel-body">
-        <iframe src="https://api.paymentwall.com/api/subscription/?key=539f0c0f95db5bbaaa064977af360820&uid={{Auth()->user()->id}}&widget=p10_1" width="750" height="800" frameborder="0"></iframe>
+<iframe name="offer_walls" id="offer_walls" src="https://www.trialpay.com/dispatch/a23ea0770fb4bc863a83874c5b96197c?sid={{Auth::user()->id}}&tph=f8246899e6b0194c&toi=Y4mVms&cn=US&hmac=e00cb5a3fe6d8bf9aebf59313acf977f&message=sid%3D2%26timestamp%3D1413891165" frameborder="0" width="100%" height="1200" scrolling="no" ></iframe>
+
 	</div>
 </div>
 
